d95b8fc73818f161a624ccf9f9d289cf5af1c66f
MuhammadAayanMirza <mmaayanmirza@gmail.com>
2026-09-26 21:21:22 +0500

    Done with all the basics of Multiplayer except the switched camera view and placement

--- Colour-Cannon-War/Assets/Scripts/NetworkPlacementManager.cs
+++ Colour-Cannon-War/Assets/Scripts/NetworkPlacementManager.cs
@@ -6,6 +6,17 @@ public class NetworkPlacementManager : NetworkBehaviour
 {
     [SerializeField] private GameObject[] placeablePrefabs;
 
+    public int GetPrefabIndex(GameObject prefab)
+    {
+        for (int i = 0; i < placeablePrefabs.Length; i++)
+        {
+            if (placeablePrefabs[i] == prefab)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
     public void RequestPlacement(int prefabIndex, Vector3 position, Quaternion rotation)
     {
         if (!IsSpawned)
--- Colour-Cannon-War/Assets/Scripts/PlacementController.cs
+++ Colour-Cannon-War/Assets/Scripts/PlacementController.cs
@@ -18,6 +18,7 @@ public class PlacementController : MonoBehaviour
 
     private readonly List<GameObject> placedObjects = new();
     private GameObject selectedPrefab;
+    [SerializeField] private NetworkPlacementManager networkPlacementManager;
     private GameObject previewObject;
     private Renderer[] previewRenderers;
     private Vector3 previewPosition;
@@ -194,11 +195,22 @@ public class PlacementController : MonoBehaviour
 
     private GameObject PlaceSelectedObject()
     {
-        GameObject placedObject = Instantiate(selectedPrefab, previewPosition, previewObject.transform.rotation);
-        placedObjects.Add(placedObject);
-        DestroyPreview();
-        selectedPrefab = null;
-        return placedObject;
+        int prefabIndex = networkPlacementManager.GetPrefabIndex(selectedPrefab);
+
+        if (prefabIndex == -1)
+        {
+            Debug.LogError("Selected prefab is not registered in NetworkPlacementManager!");
+            return null;
+        }
+        networkPlacementManager.RequestPlacement(
+            prefabIndex, 
+            previewPosition, 
+            selectedPrefab.transform.rotation);
+
+            DestroyPreview();
+            selectedPrefab = null;
+
+            return null;
     }
 
     private void SetPreviewColor(Color color)
